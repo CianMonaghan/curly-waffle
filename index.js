817be2ba8e700562d711c238df52a26a85f7861e
Cian Monaghan <ciancmonaghan@gmail.com>
2026-02-09 13:20:14 -0800

    Added node.js server that has server routes to the html page plus a ton of requisite packages

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const port = 3000;
+
+/**
+ * DATABASE
+ * TODO: Set up database
+ */
+
+/**
+ * ROUTES
+ * TODO: remove .html from pathnames after html update
+ */
+app.get('/', async (req, res) => {
+    app.use(express.static(__dirname));
+    res.sendFile(path.join(__dirname,'webpages','login.html'));
+});
+
+app.get('/account.html', async (req,res)=>{ 
+    res.sendFile(path.join(__dirname,'webpages','account.html'));
+});
+
+app.get('/character_box.html', async (req,res)=>{
+    res.sendFile(path.join(__dirname,'webpages','character_box.html'));
+});
+
+app.get('/create_character.html', async (req,res)=>{
+    res.sendFile(path.join(__dirname,'webpages','create_character.html'));
+});
+
+app.get('/sheet.html', async (req,res)=>{
+    res.sendFile(path.join(__dirname,'webpages','sheet.html'));
+});
+
+app.get('/signup.html', async (req,res)=>{
+    res.sendFile(path.join(__dirname,'webpages','signup.html'));
+});
+
+/**
+ * END ROUTES
+ */
+app.listen(port, () => {
+    console.log(`Express server listening at http://localhost:${port}`);
+});
