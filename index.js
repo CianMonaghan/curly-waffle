f65eaec1a3a2b23ef0c5c6e8bf4c769a5db695c9
Cian Monaghan <ciancmonaghan@gmail.com>
2026-02-22 20:23:08 -0800

    Added Mongoose Schema
    -Added mongoose schema to index.js
        -featureSchema (needs backend implementation)
        -backgroundSchema
        -classSchema
        -raceSchema
        -itemSchema
        -weaponSchema (currently extends itemSchema, can change if needed)
        -wonderousSchema (currently extends itemSchema, can change if needed)
        -subclassSchema
        -characterSchema (needs active features implementation)
    
    -Slight edits to some of the static_json files to match the schema
    
    WARNING: I did do some changes to the node_modules file, this might affect
    your pulls in the future - I'm sorry if this causes problems

--- index.js
+++ index.js
@@ -21,6 +21,148 @@ mongoose.connect(mongoURL)
   });
 const client = new MongoClient(mongoURL);
 
+const featureSchema = new mongoose.Schema({
+    id: { type: String, unique: true },
+    name: String,
+    description: String,
+    trigger: String,
+    //TODO: fill in here
+}, {_id: false})
+
+const backgroundSchema = new mongoose.Schema({
+    id: { type: String, unique: true, required: true },
+    name: String,
+    description: String,
+    skill_prof: [{
+        skill: String,
+        prof: Boolean
+    }],
+    tool_prof: [String],
+    lang_prof: [String],
+    features: [featureSchema]
+}, { _id: false });
+
+const classSchema = new mongoose.Schema({
+    id: { type: String, unique: true, required: true },
+    name: String,
+    caster: Boolean,
+    casterStat: String,
+    level: { type: Number, min: 0, max: 20, required: true},
+    casterLevel: Number,
+    class_hp: { type: Number, min: 0, required: true},
+    features: [featureSchema],
+    skill_prof: [{
+        skill: String,
+        prof: Boolean
+    }],
+    saving_throws: [String],
+    spell_list: [{
+        name: String,
+        url: {
+            type: String,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    // Simple regex for URL validation (can be more complex depending on requirements)
+                    return /^(ftp|http|https):\/\/[^ "]+$/.test(v); 
+                }
+            }
+        }
+    }]
+}, { _id: false });
+
+const raceSchema = new mongoose.Schema({
+    id: { type: String, unique: true, required: true},
+    name: String,
+    description: String,
+    features: [featureSchema]
+}, { _id: false });
+
+const itemSchema = new mongoose.Schema({
+    id: { type: String, unique: true, required: true},
+    local_id: {type: Number, min: 0, required: true},
+    name: String,
+    description: String,
+    equipped: Boolean,
+    features_on_obtain: [featureSchema],
+    features_on_loss: [featureSchema],
+    features_on_equip: [featureSchema],
+    features_on_unequip: [featureSchema]
+}, {_id: false});
+
+const weaponSchema = new mongoose.Schema({
+    base_item: itemSchema, //FIXME: remove this?
+    dice : {
+        oneHand: {
+            num: {type: Number, min: 1, required: true},
+            sides: {type: Number, min: 1, required: true}
+        },
+        twoHand: {
+            num: {type: Number, min: 1, required: true},
+            sides: {type: Number, min: 1, required: true}
+        }
+    },
+    range: {type: Number, min: 5},
+    primary_stat: [String],
+    martial: Boolean,
+    improvised: Boolean,
+    attack_bonus: Number,
+    damage_bonus: Number,
+    prof: true
+}, {_id: false})
+
+const wonderousSchema = new mongoose.Schema({
+    base_item: itemSchema, //FIXME: remove this?
+    attuned: Boolean,
+    features_on_attune: [featureSchema],
+    features_on_detune: [featureSchema]
+}, {_id: false})
+
+const subclassSchema = new mongoose.Schema({
+    id: { type: String, unique: true, required: true},
+    name: String,
+    class: String,
+    description: String,
+    features: [featureSchema]
+}, {_id: false})
+
+const characterSchema = new mongoose.Schema({
+    _id: {type: Number, unique: true, required: true},
+    name: {type: String, required: true},
+    alignment: String,
+    stats: {type: [{
+        stat: {type: String, unique: true, required: true},
+        score: {type: Number, min: 0, max: 30, required: true}
+    }], required: true },
+    hitpoints: {type: [{
+        hit
+    }], required: true},
+    speed: {type: Number, min: 0},
+    initiative: Number,
+    size: String,
+    prof_bonus: {type: Number, min: 2, max: 7},
+    saves: {type: [{
+        stat:  {type: String, unique: true, required: true},
+        save: {type: Number, required: true}
+    }], required: true},
+    armor_class: {type: Number, min: 1, required: true},
+    languages: [String],
+    race: raceSchema,
+    background: backgroundSchema,
+    classes: [classSchema],
+    subclasses: [subclassSchema],
+    features: [featureSchema],
+    inventory: [{
+        currency: [{
+            currencyName: String,
+            amount: {type: Number, min: 0}
+        }],
+        items: [{}]
+    }],
+    //TODO: Add active features
+    equipped_weapons: [weaponSchema]
+})
+
 /**
  * ROUTES
  * TODO: remove .html from pathnames after html update
